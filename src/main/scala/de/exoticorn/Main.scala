3275469b861393f5b28e74fc99880138307bd0af
Dennis Ranke <mail@dennisranke.de>
2012-01-15 20:57:49 +0100

    access rgba image data

--- src/main/scala/de/exoticorn/Main.scala
+++ src/main/scala/de/exoticorn/Main.scala
@@ -1,22 +1,33 @@
 import javax.imageio.ImageIO
 import java.io.File
-import java.awt.image.{ IndexColorModel, SinglePixelPackedSampleModel, DataBuffer, DataBufferByte, Raster, BufferedImage }
+import java.awt.image.{
+  IndexColorModel,
+  SinglePixelPackedSampleModel,
+  DataBuffer,
+  DataBufferByte,
+  Raster,
+  BufferedImage,
+  ColorConvertOp
+}
 import java.awt.Point
 
 object Main extends App {
   val img = ImageIO.read(new File("test.jpg"))
+  val inputImage = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_4BYTE_ABGR)
+  val convertOp = new ColorConvertOp(null)
+  convertOp.filter(img, inputImage)
 
   val paletteData = for (i <- 0 to 255; j <- 0 to 3) yield i.toByte
   val colorModel = new IndexColorModel(8, 256, paletteData.toArray, 0, true)
   val sampleModel = new SinglePixelPackedSampleModel(DataBuffer.TYPE_BYTE, img.getWidth(), img.getHeight(), Array(255))
   val outputRaster = Raster.createInterleavedRaster(DataBuffer.TYPE_BYTE, img.getWidth(), img.getHeight(), 1, new Point(0, 0))
   val dataBuffer = outputRaster.getDataBuffer().asInstanceOf[DataBufferByte]
-  val pixels = img.getData().getPixels(0, 0, img.getWidth(), img.getHeight(), null: Array[Int])
+  val pixels = inputImage.getData().getPixels(0, 0, img.getWidth(), img.getHeight(), null: Array[Int])
   for {
     x <- 0 until img.getWidth()
     y <- 0 until img.getHeight()
   } {
-    dataBuffer.getData()(x + y * img.getWidth()) = pixels((x + y * img.getWidth()) * 3).toByte
+    dataBuffer.getData()(x + y * img.getWidth()) = pixels((x + y * img.getWidth()) * 4).toByte
   }
 
   val outputImage = new BufferedImage(colorModel, outputRaster, false, null)
